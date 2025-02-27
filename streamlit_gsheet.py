2f78c40f18e00ca2c7e0e1063bba8aa100365241
thien1104 <thien0963303006@gmail.com>
2025-02-27 20:39:26 +0700

    Add files via upload

--- streamlit_gsheet.py
+++ streamlit_gsheet.py
@@ -6,6 +6,9 @@ from streamlit_gsheets import GSheetsConnection
 # Cấu hình giao diện Streamlit
 st.set_page_config(page_title="Dự đoán lưu lượng", layout="wide")
 
+# Lấy URL từ Secrets
+sheet_url = st.secrets["GOOGLE_SHEET_URL"]
+
 # Tạo kết nối đến Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 
